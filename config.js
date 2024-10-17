929bb23608a8f53f4b5fbafd7ae26c2bcd82752c
Dimuwa-000 <dimuwaofficial2005@gmail.com>
2024-10-17 08:25:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=jrwlUQBK#AR_OJVGLq0H0VqSrZWtoGU5FWTOzv3W0ZiDfcmLFT2g",
+MONGODB: process.env.MONGODB || "mongodb+srv://rygwriter2005:L9csK6k4LzPnvfIc@cluster0.fgi7g.mongodb.net/",//enter mongo db url
 };
